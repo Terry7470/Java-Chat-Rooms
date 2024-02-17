bb231c8f7c3e64aeaba28f1b9f5178388917e6c2
Terry <chenglin_terry_li@icloud.com>
2024-02-17 21:37:21 +0800

    write ClientHandler

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,6 +12,9 @@ public class Server {
     public void startServer() throws IOException {
         while(!serverSocket.isClosed()) {
             Socket socket = serverSocket.accept();
+            ClientHandler clientHandler = new ClientHandler(socket);
+            Thread thread = new Thread(clientHandler);
+
         }
     }
 
@@ -19,6 +22,30 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(1004);
         Server server = new Server(serverSocket);
         server.startServer();
+    }
+
+}
+
+class ClientHandler {
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private String clientUsername;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        this.clientUsername = bufferedReader.readLine();
+        clientHandlers.add(this);
+    }
+
+    public void run() throws IOException {
+        messageFromClient = bufferedReader.readLine();
+        BroadcastMessages(messageFromClient);
+    }
+
+    public void BroadcastMessages(String messageToSend) {
+
     }
 
 }
