1fbe1ea8a475a7027e1083c11b83bb3295222668
Terry <chenglin_terry_li@icloud.com>
2024-02-17 20:13:15 +0800

    basic server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,24 @@
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 import java.io.*;
 
 public class Server {
-    
+    private ServerSocket serverSocket;
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() throws IOException {
+        while(!serverSocket.isClosed()) {
+            Socket socket = serverSocket.accept();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1004);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
 }
